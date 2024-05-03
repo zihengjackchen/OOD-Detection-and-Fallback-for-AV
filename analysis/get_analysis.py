0aab59b71cf1a6a79f27b884f62a9ffdca8e6447
hermya <herambku@gmail.com>
2024-05-03 16:43:04 -0500

    Added mechanism to print average lane shifing

--- analysis/get_analysis.py
+++ analysis/get_analysis.py
@@ -9,19 +9,27 @@ pd.set_option('display.max_colwidth', None)
 MAHA = 'maha'
 BASELINE = 'baseline'
 # reading csv file 
-df = pd.read_csv("maharesults.csv")
-# print(df.head())
-csv_rows = []
-csv_rows.append(",".join(["scenario", "scenario_type_intensity", "route_change", "collisions_vehicle", "collisions_layout"]))
-scenarios = ['haze', 'shade', 'rain']
-for scenario in scenarios:
-    for scenario_intensity in range(0, 10):
-        route_change = df.loc[(df['scenario_type'] == scenario) & (df['scenario_type_intensity'] == scenario_intensity) & (pd.notna(df['outside_route_lanes']))].describe().loc['count'][0]
-        collisions_vehicle = df.loc[(df['scenario_type'] == scenario) & (df['scenario_type_intensity'] == scenario_intensity) & (pd.notna(df['collisions_vehicle']))].describe().loc['count'][0]
-        collisions_layout = df.loc[(df['scenario_type'] == scenario) & (df['scenario_type_intensity'] == scenario_intensity) & (pd.notna(df['collisions_layout']))].describe().loc['count'][0]
-        csv_rows.append(",".join([str(scenario), str(scenario_intensity), str(route_change/0.11), str(collisions_vehicle/0.11), str(collisions_layout/0.11)]))
-        # print(scenario, scenario_intensity, route_change/0.11, collisions_vehicle/0.11, collisions_layout/0.11)
-    #print(df.loc[:,['scenario_num', 'scenario_type_intensity', 'collisions_vehicle', 'collisions_layout', 'outside_route_lanes']]) 
+def get_aggr_result_csv_string(result_file_type):
+    df = pd.read_csv(result_file_type+"results.csv")
+    # print(df.head())
+    csv_rows = []
+    csv_rows.append(",".join(["scenario", "scenario_type_intensity", "route_change", "route_change_avg", "collisions_vehicle", "collisions_layout"]))
+    scenarios = ['haze', 'shade', 'rain']
+    for scenario in scenarios:
+        for scenario_intensity in range(0, 10):
+            route_change = df.loc[(df['scenario_type'] == scenario) & (df['scenario_type_intensity'] == scenario_intensity) & (pd.notna(df['outside_route_lanes']))]['outside_route_lanes'].describe().loc['count']
+            #print(df.loc[(df['scenario_type'] == scenario) & (df['scenario_type_intensity'] == scenario_intensity) & (pd.notna(df['outside_route_lanes']))]['outside_route_lanes'].describe())
+            collisions_vehicle = df.loc[(df['scenario_type'] == scenario) & (df['scenario_type_intensity'] == scenario_intensity) & (pd.notna(df['collisions_vehicle']))]['collisions_vehicle'].describe().loc['count']
+            #print(df.loc[(df['scenario_type'] == scenario) & (df['scenario_type_intensity'] == scenario_intensity) & (pd.notna(df['collisions_vehicle']))]['collisions_vehicle'].describe())
+            collisions_layout = df.loc[(df['scenario_type'] == scenario) & (df['scenario_type_intensity'] == scenario_intensity) & (pd.notna(df['collisions_layout']))]['collisions_layout'].describe().loc['count']
+            #print(df.loc[(df['scenario_type'] == scenario) & (df['scenario_type_intensity'] == scenario_intensity) & (pd.notna(df['collisions_layout']))]['collisions_layout'].describe())
+            route_change_avg = df.loc[(df['scenario_type'] == scenario) & (df['scenario_type_intensity'] == scenario_intensity)]['outside_route_lanes_average'].describe().loc['mean']
+            #print(df.loc[(df['scenario_type'] == scenario) & (df['scenario_type_intensity'] == scenario_intensity)].describe())
+            csv_rows.append(",".join([str(scenario), str(scenario_intensity), "{:.2f}".format(route_change/0.11), "{:.2f}".format(route_change_avg), "{:.2f}".format(collisions_vehicle/0.11), "{:.2f}".format(collisions_layout/0.11)]))
+            # print(scenario, scenario_intensity, route_change/0.11, collisions_vehicle/0.11, collisions_layout/0.11)
+        #print(df.loc[:,['scenario_num', 'scenario_type_intensity', 'collisions_vehicle', 'collisions_layout', 'outside_route_lanes']]) 
+    return "\n".join(csv_rows)
 
-with open(BASELINE + 'aggregate.csv', 'w') as result_file:
-    result_file.write("\n".join(csv_rows))
+result_file_type = BASELINE
+with open(result_file_type + 'aggregate.csv', 'w') as result_file:
+    result_file.write(get_aggr_result_csv_string(result_file_type))
--- analysis/read_json_save_csv.py
+++ analysis/read_json_save_csv.py
@@ -1,12 +1,13 @@
 import os
 import json
+import re
 # This file tells us the number of values contained in the json at max
 PREFIX = 'paramsweep_results_lead_slowdown_weather_'
 BASELINE = 'baseline'
 MAHA = 'maha'
 TEST_FILE_COMMON_NAME = "/results_summary.json"
 TEST_FILE_ROOT = "../testing/"
-
+PERCENT_SEARCH = re.compile(r'\(.*%')
 
 def get_csv_file_string(test_results_dir):
     path = TEST_FILE_ROOT + PREFIX + test_results_dir
@@ -17,6 +18,7 @@ def get_csv_file_string(test_results_dir):
                 "collisions_vehicle",\
                 "collisions_layout",\
                 "outside_route_lanes",\
+                "outside_route_lanes_average",\
                 "meta_duration_game",\
                 "meta_duration_system",\
                 "meta_route_length",\
@@ -40,6 +42,9 @@ def get_csv_file_string(test_results_dir):
             collisions_layout = collisions_layout.replace(",", " ")
             outside_route_lanes = infractions['outside_route_lanes'][0] if len(infractions['outside_route_lanes']) > 0 else ""
             outside_route_lanes = outside_route_lanes.replace(",", " ")
+            outside_route_lanes_average = '0.0'
+            if (outside_route_lanes.strip()):
+                outside_route_lanes_average = PERCENT_SEARCH.search(outside_route_lanes).group()[1:-1]
             scenario_num = file_name_split[5][2:]
             scenario_type = file_name_split[6]
             scenario_type_intesnsity = file_name_split[7][2]
@@ -56,6 +61,7 @@ def get_csv_file_string(test_results_dir):
                 collisions_vehicle,\
                 collisions_layout,\
                 outside_route_lanes,\
+                outside_route_lanes_average,\
                 meta_duration_game,\
                 meta_duration_system,\
                 meta_route_length,\
@@ -67,7 +73,6 @@ def get_csv_file_string(test_results_dir):
             csv_rows.append(",".join(cur))
     return "\n".join(csv_rows)
 
-# if (__name__ == "main" ):
-#     print("Saving ... results.csv")
-with open(BASELINE + 'results.csv', 'w') as result_file:
-    result_file.write(get_csv_file_string(MAHA))
+result_type = BASELINE
+with open(result_type + 'results.csv', 'w') as result_file:
+    result_file.write(get_csv_file_string(result_type))
